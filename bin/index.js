559713721a071011ed0e61d714b7515f30b0d2c9
Piotrko64 <PiotrPc77@gmail.com>
2023-08-21 11:56:47 +0200

    Add License Information

--- bin/index.js
+++ bin/index.js
@@ -1,27 +1,29 @@
 #! /usr/bin/env node
 
-const { displayRcliLogo } = require("./helpers/displayRcliLogo");
-const { createEntireComponent } = require("./helpers/createEntireComponent");
-const { program } = require("commander");
-const { getStyleMode } = require("./helpers/forStyles/getStyleMode");
+const { displayRcliLogo } = require('./helpers/displayRcliLogo');
+const { createEntireComponent } = require('./helpers/createEntireComponent');
+const { program } = require('commander');
+const { getStyleMode } = require('./helpers/forStyles/getStyleMode');
 
 program
-    .argument("<pathFile>")
-    .name("rc")
-    .usage("<pathFile> [options]")
-    .option("-j, --jsx", "Create .jsx file instead .tsx")
-    .option("-t, --tsx", "Create .tsx file [DEFAULT]")
-    .option("-ns, --no-style", "Don't create file for style")
-    .option("-nm, --no-module", "Create style file without module (for example .css instead .module.css)")
-    .option("-m, --module", "Create file as module [DEFAULT]")
-    .option("-sc, --scss", "Create .module.scss or .scss file instead .css")
-    .option("-cs, --css", "Create .css/.module.css file [DEFAULT]")
-    .option("-sa, --sass", "Create .module.sass or .sass file instead .css")
-    .option("-sty, --styled-components", "Create styled components file for style")
-    .option("-h, --hook", "Create also file for hook")
-    .option("-nf, --no-folder", "Create component files without folder")
+    .argument('<pathFile>')
+    .name('rc')
+    .usage('<pathFile> [options]')
+    .option('-j, --jsx', 'Create .jsx file instead .tsx')
+    .option('-t, --tsx', 'Create .tsx file [DEFAULT]')
+    .option('-ns, --no-style', "Don't create file for style")
+    .option('-nm, --no-module', 'Create style file without module (for example .css instead .module.css)')
+    .option('-m, --module', 'Create file as module [DEFAULT]')
+    .option('-sc, --scss', 'Create .module.scss or .scss file instead .css')
+    .option('-cs, --css', 'Create .css/.module.css file [DEFAULT]')
+    .option('-sa, --sass', 'Create .module.sass or .sass file instead .css')
+    .option('-sty, --styled-components', 'Create styled components file for style')
+    .option('-h, --hook', 'Create also file for hook')
+
+    .option('-nf, --no-folder', 'Create component files without folder')
 
     .action((pathFile, options) => {
+        console.log(options);
         const { hook, style, module, jsx, folder } = options;
 
         displayRcliLogo();
